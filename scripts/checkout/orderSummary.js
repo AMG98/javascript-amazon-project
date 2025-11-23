bd6b1b884258fcf2396dc8ad7d243c1b91143622
AMG98 <amirgrzybowski@outlook.com>
2025-11-23 12:45:33 +0100

    15h

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -155,12 +155,12 @@ export function renderOrderSummary() {
         const productId = link.dataset.productId;
         removeFromCart(productId);
 
-        const container = document.querySelector(`.js-cart-item-container-${productId}`);
-        container.remove();
+        renderOrderSummary();
 
         updateCartQuantity();
 
         renderPaymentSummary();
+        
       });
     });
 
